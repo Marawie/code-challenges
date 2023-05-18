95630c4b30d48eea9d3523ad5526645644765aa4
Marawie <109871213+Marawie@users.noreply.github.com>
2023-05-18 23:21:52 +0200

    Update DoubleChar.java

--- DoubleChar.java
+++ DoubleChar.java
@@ -1,5 +1,6 @@
 public class DoubleChar {
 // https://www.codewars.com/kata/56b1f01c247c01db92000076
+    
     public String doubleChar(String s){
         StringBuilder stringBuilder = new StringBuilder();
         char[] stringToChar = s.toCharArray();

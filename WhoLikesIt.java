96e94e870fd5cfa4bc67f2bd2765da35133d30a5
Marawie <109871213+Marawie@users.noreply.github.com>
2023-05-18 23:18:22 +0200

    Update WhoLikesIt.java

--- WhoLikesIt.java
+++ WhoLikesIt.java
@@ -1,13 +1,10 @@
-
 public class WhoLikesIt {
-
-    /*.[]                                -->  "no one likes this"
-    ["Peter"]                         -->  "Peter likes this"
-    ["Jacob", "Alex"]                 -->  "Jacob and Alex like this"
-    ["Max", "John", "Mark"]           -->  "Max, John and Mark like this"
-    ["Alex", "Jacob", "Mark", "Max"]  -->  "Alex, Jacob and 2 others like this"*/
-    public static String whoLikesIt(String... names) {
-        switch (names.length){
+    
+    //https://www.codewars.com/kata/5266876b8f4bf2da9b000362/
+    
+    public String whoLikesIt(String... names) {
+        switch (names.length)
+        {
             case 0:return "no one likes this";
             case 1:return names[0]+" likes this";
             case 2:return names[0]+" and "+names[1]+" like this";
@@ -15,5 +12,4 @@ public class WhoLikesIt {
             default:return names[0]+", "+names[1]+" and "+(names.length-2)+" others like this";
         }
     }
-
 }

eb06e1fa0e3b057888c8c3e9d95a1215078bd1cc
Marawie <109871213+Marawie@users.noreply.github.com>
2023-05-18 22:41:36 +0200

    Create CalculateAvg.java

---
+++ CalculateAvg/CalculateAvg.java
@@ -0,0 +1,22 @@
+import java.util.Arrays;
+
+public class CalculateAvg {
+
+    //Write a function which calculates the average of the numbers in a given list.
+    //Note: Empty arrays should return 0.
+    public static double find_average1(int[] array){
+        double avg = Arrays.stream(array).sum() / (double) array.length;
+        return array.length == 0 ? 0 : avg;
+    }
+
+    public static double find_average2(int[] array){
+        double sum = 0;
+        for (Integer numb : array){
+            sum += numb;
+        }
+        double avg = sum / array.length;
+    return array.length == 0? 0 : avg;
+    }
+
+
+}

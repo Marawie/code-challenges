8845ee34fe63087cd30df988c7cd1b6164a54235
Marawie <109871213+Marawie@users.noreply.github.com>
2023-05-19 15:34:34 +0200

    Update SortArrayAscending.java

--- SortArrayAscending/SortArrayAscending.java
+++ SortArrayAscending/SortArrayAscending.java
@@ -1,17 +1,15 @@
-package SortArrayAscending;
-import java.util.List;
 public class SortArrayAscending {
-    public List<Integer> ascList(List<Integer> list) {
-        for (int i = 0; i < list.size(); i++) {
-            for (int j = i + 1; j < list.size(); j++) {
-                int numbA = list.get(i);
-                int numbB = list.get(j);
+    public int[] ascArr(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = i + 1; j < arr.length; j++) {
+                int numbA = arr[i];
+                int numbB = arr[j];
                 if (numbA > numbB) {
-                    list.set(i, numbB);
-                    list.set(j, numbA);
+                arr[i] = numbB;
+                arr[j] = numbA;
                 }
             }
         }
-        return list;
+        return arr;
     }
 }

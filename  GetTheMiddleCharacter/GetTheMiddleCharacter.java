68931013eb94be1eebeca923d607768f525e5ba2
Marek Marczak <marawieeee@gmail.com>
2023-05-21 22:27:00 +0200

    The first think I saw that if word length is even i need to return 2 char in the middle if not only 1, but if the word has only 2 char need to return the whole word

---
+++  GetTheMiddleCharacter/GetTheMiddleCharacter.java
@@ -0,0 +1,14 @@
+public class GetTheMiddleCharacter {
+    //https://www.codewars.com/kata/56747fd5cb988479af000028
+    public static String getMiddle(String word) {
+        if (word.length() <= 2) {
+            return word;
+        } else if (word.length() % 2 == 0) {
+            int middleIndex = word.length() / 2;
+            return word.substring(middleIndex - 1, middleIndex + 1);
+        } else {
+            int middleIndex = word.length() / 2;
+            return word.substring(middleIndex, middleIndex + 1);
+        }
+    }
+}

25f50050f388da71327c047b7ec152005539ac8b
Marek Marczak <marawieeee@gmail.com>
2023-05-22 20:51:05 +0200

    Should only return 4 last numbers, all rest number should be #, was nice task to solve.

---
+++ CreditCardMask/CreditCardMask.java
@@ -0,0 +1,16 @@
+public class CreditCardMask {
+    // https://www.codewars.com/kata/5412509bd436bd33920011bc/
+
+    public static String maskify(String str) {
+        StringBuilder sb = new StringBuilder();
+        int length = str.length();
+        if (length <= 3) {
+            return str;
+        } else
+            for (int i = 0; i < length - 4; i++) {
+                sb.append("#");
+            }
+        sb.append(str, length - 4, length);
+        return sb.toString();
+    }
+}
---
+++ JadenCasingStrings/JadenCasingStrings.java
@@ -0,0 +1,30 @@
+public class JadenCasingStrings {
+    // https://www.codewars.com/kata/5390bac347d09b7da40006f6
+
+    public String toJadenCase(String phrase) {
+        if (phrase == null || phrase.length() == 0) {
+            return null;
+        }
+        StringBuilder sb = new StringBuilder();
+        boolean capitalizeNext = true;
+
+        for (int i = 0; i < phrase.length(); i++) {
+            char c = phrase.charAt(i);
+
+            if (Character.isWhitespace(c)) {
+                capitalizeNext = true;
+                sb.append(c);
+            } else {
+                if (capitalizeNext) {
+                    c = Character.toUpperCase(c);
+                    capitalizeNext = false;
+                } else {
+                    c = Character.toLowerCase(c);
+
+                }
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}

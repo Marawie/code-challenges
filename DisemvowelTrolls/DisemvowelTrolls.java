d5812a6ba82a75a6de43fe2f21ecdb8bf8d76718
Marek Marczak <marawieeee@gmail.com>
2023-05-22 19:05:53 +0200

    I implement this manually instead of using method replace()

---
+++ DisemvowelTrolls/DisemvowelTrolls.java
@@ -0,0 +1,15 @@
+public class DisemvowelTrolls {
+    // https://www.codewars.com/kata/52fba66badcd10859f00097e/
+
+    public static String disemvowel(String str) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (!(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ||
+                    c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')) {
+                sb.append(c);
+            }
+        }
+        return sb.toString();
+    }
+}

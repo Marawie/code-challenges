b22064e8090f97d032821fb1273fbab475b72476
MarekMarczak <marawieeee@gmail.com>
2023-06-05 17:02:47 +0200

    rename the methods

--- CalculateAvg/CalculateAvg.java
+++ CalculateAvg/CalculateAvg.java
@@ -2,12 +2,12 @@ import java.util.Arrays;
 public class CalculateAvg {
     // https://www.codewars.com/kata/57a2013acf1fa5bfc4000921
 
-    public double findAverage1(int[] array) {
+    public double findAverage(int[] array) {
         double avg = Arrays.stream(array).sum() / (double) array.length;
         return array.length == 0 ? 0 : avg;
     }
 
-    public double findAverage2(int[] array) {
+    public double findAverageAnotherWay(int[] array) {
         double sum = 0;
         for (Integer numb : array) {
             sum += numb;

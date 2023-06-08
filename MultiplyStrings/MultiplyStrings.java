1b425ff2d6dfa460d60d94a3e0d925472f36b3c1
MarekMarczak <marawieeee@gmail.com>
2023-06-08 23:28:33 +0200

    small change

--- MultiplyStrings/MultiplyStrings.java
+++ MultiplyStrings/MultiplyStrings.java
@@ -4,7 +4,7 @@ public class MultiplyStrings {
     public String multiply(String num1, String num2) {
         BigDecimal bigDecimal1 = new BigDecimal(num1);
         BigDecimal bigDecimal2 = new BigDecimal(num2);
-        BigDecimal sum = bigDecimal1.multiply(bigDecimal2);
-        return String.valueOf(sum);
+        BigDecimal multiply = bigDecimal1.multiply(bigDecimal2);
+        return String.valueOf(multiply);
     }
 }

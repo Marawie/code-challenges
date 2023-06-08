baca714dbc74ac9f49e881560c42c1a61a2106e7
MarekMarczak <marawieeee@gmail.com>
2023-06-08 23:27:47 +0200

    small change

--- Quadrants/Quadrants.java
+++ Quadrants/Quadrants.java
@@ -2,6 +2,6 @@ public class Quadrants {
     //https://www.codewars.com/kata/643af0fa9fa6c406b47c5399/train/java
 
     public int quadrant(int x, int y) {
-        return x>0 && y>0? 1 : x>0 && y<0? 4 : x<0 && y>0? 2 : x<0 && y<0?  3 : 0;
+        return (x > 0 && y > 0) ? 1 : (x > 0 && y < 0) ? 4 : (x < 0 && y > 0) ? 2 : (x < 0 && y < 0) ? 3 : 0;
     }
 }

399945e961ec41195ec86922355c167e7e8e79c5
MarekMarczak <marawieeee@gmail.com>
2023-06-11 23:31:11 +0200

    now should be better

--- CardGame/CardGame.java
+++ CardGame/CardGame.java
@@ -1,34 +1,40 @@
 public class CardGame {
     //https://www.codewars.com/kata/60576b180aef19001bce494d
     public String winner(String[] deckSteve, String[] deckJosh) {
-        int stevePoints = 0;
-        int joshPoints = 0;
         int lengthOfDecks = deckJosh.length;
-        for (int i = 0; i < lengthOfDecks; i++) {
-            deckSteve[i] = deckSteve[i].replace("A", "14");
-            deckSteve[i] = deckSteve[i].replace("K", "13");
-            deckSteve[i] = deckSteve[i].replace("Q", "12");
-            deckSteve[i] = deckSteve[i].replace("J", "11");
-            deckSteve[i] = deckSteve[i].replace("T", "10");
-            deckJosh[i] = deckJosh[i].replace("A", "14");
-            deckJosh[i] = deckJosh[i].replace("K", "13");
-            deckJosh[i] = deckJosh[i].replace("Q", "12");
-            deckJosh[i] = deckJosh[i].replace("J", "11");
-            deckJosh[i] = deckJosh[i].replace("T", "10");
-        }
-        for (int i = 0; i < lengthOfDecks; i++) {
-            if (Integer.parseInt(deckJosh[i]) > Integer.parseInt(deckSteve[i])) {
-                joshPoints++;
-            } else if (Integer.parseInt(deckJosh[i]) < Integer.parseInt(deckSteve[i])) {
-                stevePoints++;
+        int scoreSteve = 0, scoreJosh = 0;
+        for (int i = 0; i < lengthOfDecks; ++i) {
+            int cardSteve = getCardValue(deckSteve[i]);
+            int cardJosh = getCardValue(deckJosh[i]);
+            if (cardSteve > cardJosh) {
+                scoreSteve++;
+            } else if (cardSteve < cardJosh) {
+                scoreJosh++;
             }
         }
-
-        if (joshPoints > stevePoints) {
-            return "Josh wins " + joshPoints + " to " + stevePoints;
-        } else if (stevePoints > joshPoints) {
-            return "Steve wins " + stevePoints + " to " + joshPoints;
+        if (scoreSteve == scoreJosh) {
+            return "Tie";
+        } else if (scoreSteve > scoreJosh) {
+            return "Steve wins " + scoreSteve + " to " + scoreJosh;
+        } else {
+            return "Josh wins " + scoreJosh + " to " + scoreSteve;
         }
-        return "Tie";
+    }
+
+    private int getCardValue(String card) {
+        switch (card) {
+            case "T":
+                return 10;
+            case "J":
+                return 11;
+            case "Q":
+                return 12;
+            case "K":
+                return 13;
+            case "A":
+                return 14;
+            default:
+                return Integer.parseInt(card);
+        }
     }
 }

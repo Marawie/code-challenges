dbf187425532174c5268638d9b8689ea49a16e81
MarekMarczak <marawieeee@gmail.com>
2023-06-22 23:36:59 +0200

    use brute force algorithm to search through the array and find the answer(if arrays, are not big this algorithm is fine, but has huge 0(n), so be careful.

--- LongestPalindromicSubstring/LongestPalindromicSubstring.java
+++ LongestPalindromicSubstring/LongestPalindromicSubstring.java
@@ -8,8 +8,8 @@ public class LongestPalindromicSubstring {
         int start = 0;
         int end = 0;
         for (int i = 0; i < length; i++) {
-            int len1 = expandAroundCenter(s, i, i);
-            int len2 = expandAroundCenter(s, i, i + 1);
+            int len1 = getPalindromeLength(s, i, i);
+            int len2 = getPalindromeLength(s, i, i + 1);
             int len = Math.max(len1, len2);
             if (len > end - start) {
                 start = i - (len - 1) / 2;
@@ -19,7 +19,7 @@ public class LongestPalindromicSubstring {
         return s.substring(start, end + 1);
     }
 
-    public int expandAroundCenter(String s, int left, int right) {
+    public int getPalindromeLength(String s, int left, int right) {
         int length = s.length();
         while (left >= 0 && right < length && s.charAt(left) == s.charAt(right)) {
             left--;
